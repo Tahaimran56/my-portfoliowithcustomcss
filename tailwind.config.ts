1a6b2d5e5ab1a9932c22cdd097450f897e210f30
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2024-11-08 00:33:01 +0500

    Add files via upload
    this is my  simple portfolio more updates come that is just basic with custom css

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
